99e6bcb5df5c03269ab30a1be53b612df260ac33
roger067 <rosestolato.rogerio@gmail.com>
2024-01-16 23:07:52 -0300

    core: remove radix ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Theme } from '@radix-ui/themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Router from './routes';
@@ -14,9 +13,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Theme>
-        <Router />
-      </Theme>
+      <Router />
     </QueryClientProvider>
   </React.StrictMode>
 );
