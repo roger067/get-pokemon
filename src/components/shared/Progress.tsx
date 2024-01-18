af7cb18b9de0276a4fad4faf98afacdc5ce5357d
roger067 <rosestolato.rogerio@gmail.com>
2024-01-18 08:52:13 -0300

    test: create progress test

--- src/components/shared/Progress.tsx
+++ src/components/shared/Progress.tsx
@@ -16,6 +16,7 @@ const Progress: React.FC<ProgressProps> = ({ label, value }) => {
       <div className="w-full bg-gray-200 rounded-full h-2.5">
         <div
           className="bg-indigo-600 h-2.5 rounded-full"
+          role="progressbar"
           style={{ width: progressValue }}
         ></div>
       </div>
--- src/components/shared/__tests__/Progress.spec.tsx
+++ src/components/shared/__tests__/Progress.spec.tsx
@@ -1,7 +1,21 @@
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import { describe, expect, test } from 'vitest';
+
 import Progress from '../Progress';
 
-test('Renders the main page', () => {
-  render(<Progress value={10} label="Teste" />);
-  expect(true).toBeTruthy();
+describe('test progress bar', () => {
+  test('should render correct label', () => {
+    render(<Progress value={75} label="Attack" />);
+    expect(screen.getByText(/Attack/)).toBeInTheDocument();
+    expect(screen.getByText(/75/)).toBeInTheDocument();
+  });
+
+  test('should render progress width based on value', () => {
+    render(<Progress value={75} label="Attack" />);
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+    expect(screen.getByRole('progressbar')).toHaveStyle(`width: 50%`);
+  });
 });
